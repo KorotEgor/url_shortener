82162b429b64b2fdca68989684e142642bfed52c
korotkov_egor <korotkovegor10@gmail.com>
2025-06-09 20:03:33 +0300

    add form for user url

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -1,5 +1,11 @@
-from views import home
+from views import home_get, home_post
+from aiohttp import web
 
 
 def setup_routes(app):
-    app.router.add_get("/", home)
+    app.add_routes(
+        [
+            web.get("/", home_get),
+            web.post("/", home_post),
+        ]
+    )
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,6 +1,17 @@
 import aiohttp_jinja2
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @aiohttp_jinja2.template("home.html")
-async def home(request):
+async def home_get(request):
+    return {}
+
+
+@aiohttp_jinja2.template("home.html")
+async def home_post(request):
+    data = await request.post()
+    user_url = data["user_url"]
+    logger.info(f"user_url: {user_url}")
     return {}
