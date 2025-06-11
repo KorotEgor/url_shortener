fa303012236294957c2dd03073e6b3be5e0c9f92
korotkov_egor <korotkovegor10@gmail.com>
2025-06-11 10:37:31 +0300

    add new url creater

---
+++ url_shortener/trans_url.py
@@ -0,0 +1,26 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TransUrl:
+    def __init__(self):
+        self.url_chars = (
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        )
+        self.last_coomb = "http://localhost:8080/"
+        self.last_ch_num = 1
+        self.last_added_ch_num = 0
+
+    async def get_short_url(self):
+        new_char = self.url_chars[self.last_ch_num]
+
+        if self.last_ch_num == 0:
+            self.last_coomb += self.url_chars[self.last_added_ch_num]
+            self.last_added_ch_num += 1
+
+        self.last_ch_num = (self.last_ch_num + 1) % 62
+
+        new_url = self.last_coomb + new_char
+        logger.info(f"new_url: {new_url}")
+        return new_url
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,7 +1,11 @@
 import aiohttp_jinja2
 import logging
+from aiohttp import web
 
 from url_shortener.validator import validate_url
+from url_shortener.trans_url import TransUrl
+
+tr_url = TransUrl()
 
 logger = logging.getLogger(__name__)
 
@@ -11,12 +15,27 @@ async def home(request):
     if request.method == "POST":
         data = await request.post()
         user_url = data["user_url"]
+        logger.info(f"user_url: {user_url}")
 
         is_cor_url = await validate_url(user_url)
         if not is_cor_url:
             return {"bad_url": True}
 
-        logger.info(f"user_url: {user_url}")
-        return {"new_url": "http//:localhost:8080/urls/"}
+        new_url = await tr_url.get_short_url()
+        return {"new_url": new_url}
 
     return {}
+
+
+@aiohttp_jinja2.template("urls.html")
+async def urls(request):
+    url = request.match_info.get("url", "bad_url")
+    logger.info(f"url: {url}")
+
+    # достаем из базы соотв url
+    redirect_url = "https://docs.aiohttp.org/en/stable/web_quickstart.html"
+
+    if redirect_url is None:
+        redirect_url = "http//:localhost:8080/"
+
+    raise web.HTTPFound(redirect_url)
