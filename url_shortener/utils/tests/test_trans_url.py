0b26f34d646735bdf4a31c98d20d1b6daa09dfa7
korotkov_egor <korotkovegor10@gmail.com>
2025-06-22 21:45:42 +0300

    add test for TransUrl with last coomb

--- url_shortener/utils/tests/test_trans_url.py
+++ url_shortener/utils/tests/test_trans_url.py
@@ -2,7 +2,7 @@ from url_shortener.utils.trans_url import TransUrl, get_last_coomb
 from unittest.mock import AsyncMock
 
 
-async def test_get_short_url():
+async def test_get_short_url_empty():
     tr_url = TransUrl()
     new_url1 = await tr_url.get_short_url()
     assert new_url1 == "http://localhost:8080/urls/b"
@@ -15,6 +15,20 @@ async def test_get_short_url():
 
     new_url63 = await tr_url.get_short_url()
     assert new_url63 == "http://localhost:8080/urls/ab"
+
+
+async def test_get_short_url_data():
+    tr_url = TransUrl("c")
+
+    assert tr_url.last_coomb == "http://localhost:8080/urls/"
+    assert tr_url.last_ch_num == 3
+    assert tr_url.last_added_ch_num == 0
+
+    tr_url = TransUrl("cc")
+
+    assert tr_url.last_coomb == "http://localhost:8080/urls/c"
+    assert tr_url.last_ch_num == 3
+    assert tr_url.last_added_ch_num == 2
 
 
 async def test_get_last_coomb():
